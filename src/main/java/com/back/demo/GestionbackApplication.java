737f8d7de6bdd452e025fbae7554786268ea83a6
Abdou0210 <abdouraoufsaid025@gmail.com>
2020-03-01 23:49:45 +0000

    envoyé

---
+++ src/main/java/com/back/demo/GestionbackApplication.java
@@ -0,0 +1,13 @@
+package com.back.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionbackApplication.class, args);
+	}
+
+}
